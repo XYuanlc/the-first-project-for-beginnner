0ec005dacfca252e38c437ef0aeb98e79b958ca9
Xyuan <2502155017@qq.com>
2023-05-12 21:58:03 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void print(int(*p)[5], int r, int c)
+{
+	int i = 0;
+	for (i = 0; i < r; i++)
+	{
+		int j = 0;
+		for (j = 0; j < c; j++)
+		{
+			printf("%d ", *(*(p + i) + j));
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int arr[3][5] = { 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
+	print(arr, 3, 5);
+}
